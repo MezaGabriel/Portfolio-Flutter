35f23c986d0dee7843a3e5de0adce47a03a69a36
Gabriel Meza <96127228+MezaGabriel@users.noreply.github.com>
2022-12-10 17:23:04 -0300

    Flutter Designs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:designs/screens/basic_design.dart';
+import 'package:designs/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,8 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basic_design',
-      routes: {'basic_design': (_) => BasicDesignScreen()},
+      initialRoute: 'scroll_screen',
+      routes: {
+        'basic_design': (_) => BasicDesignScreen(),
+        'scroll_screen': (_) => ScrollScreen(),
+      },
     );
   }
 }
