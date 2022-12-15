35b2001dbd61c329ff4ee65fe9176c53589cd421
Gabriel Meza <96127228+MezaGabriel@users.noreply.github.com>
2022-12-15 17:40:43 -0300

    User Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/screens.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,15 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: HomeScreen.routerName,
+      routes: {
+        HomeScreen.routerName: (_) => const HomeScreen(),
+        SettingsScreen.routerName: (_) => const SettingsScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:userpreferences/screens/home_screen.dart';
+export 'package:userpreferences/screens/settings_screen.dart';
