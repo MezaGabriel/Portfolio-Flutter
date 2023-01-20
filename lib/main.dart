bd7757de1e7900480d69833425e23f035d8c8cfd
MezaGabriel <mez4.gabriel@gmail.com>
2023-01-19 23:12:54 -0300

    Push Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,41 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> messengerKey =
+      new GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // Context!
+    PushNotificationService.messageStream.listen((message) {
+      // print('MyApp: $message');
+      navigatorKey.currentState?.pushNamed('message', arguments: message);
+      final snackBar = SnackBar(content: Text(message));
+      messengerKey.currentState?.showSnackBar(snackBar);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'home',
+      navigatorKey: navigatorKey, // Navigate
+      scaffoldMessengerKey: messengerKey, // Snacks
       routes: {
         'home': (_) => HomeScreen(),
         'message': (_) => MessageScreen(),
--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,19 +1,51 @@
 // SHA1: 0A:C2:76:C6:50:91:6C:8A:FB:5D:9B:61:F7:60:52:C2:F3:D0:AD:B1
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static StreamController<String> _messageStream =
+      new StreamController.broadcast();
+  static Stream<String> get messageStream => _messageStream.stream;
+
+  static Future _backgroundHandler(RemoteMessage message) async {
+    // print('background Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['product'] ?? 'No data');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    // print('onMessage Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['product'] ?? 'No data');
+  }
+
+  static Future _onMessageOpenApp(RemoteMessage message) async {
+    // print('onMessageOpenApp Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['product'] ?? 'No data');
+  }
 
   static Future initializedApp() async {
     // Push Notification
 
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
-    print(token);
+    print('Token: ${token}');
+
+    // Handlers
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
 
     // Local Notification
+  }
+
+  static closeStreams() {
+    _messageStream.close();
   }
 }
