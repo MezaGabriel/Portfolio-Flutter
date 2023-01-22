535664ebe105c40e8199f5c8b7ad141ee067e94b
MezaGabriel <mez4.gabriel@gmail.com>
2023-01-22 18:15:39 -0300

    MapboxGL

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-class FullScreenMap extends StatelessWidget {
+class FullScreenMap extends StatefulWidget {
+  @override
+  State<FullScreenMap> createState() => _FullScreenMapState();
+}
+
+class _FullScreenMapState extends State<FullScreenMap> {
+  MapboxMapController? mapController;
+
+  void _onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('FullScreenMap'),
+      body: MapboxMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(-26.814585, -65.225724),
+          zoom: 14,
+        ),
       ),
     );
   }
